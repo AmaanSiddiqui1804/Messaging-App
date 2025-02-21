dec04f3d4dc25b0592e4cf209b51cc713cf56812
AmaanSiddiqui1804 <amaansiddiqui1804@gmail.com>
2025-02-21 15:20:03 +0530

    Amaan Siddiqui: Add. UC2 Query functionality

--- src/main/java/com/example/uc1/hello_api/HelloController.java
+++ src/main/java/com/example/uc1/hello_api/HelloController.java
@@ -1,13 +1,23 @@
 package com.example.uc1.hello_api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 public class HelloController {
+
+    //UC1                                    http://localhost:8080/hello
     @GetMapping(value = {"/uc1", "/hello"})
     public String sayHello() {
         return "Hello from BridgeLabz";
     }
+
+    //UC2 Passing name as query parameter    http://localhost:8080/query?name=Amaan
+    @GetMapping("/query")
+    public String sayHelloQuery(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+
 }
