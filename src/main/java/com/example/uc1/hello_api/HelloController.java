c6bc28eb389ff6cb7ad5c6fbfd41aff886babfe0
AmaanSiddiqui1804 <amaansiddiqui1804@gmail.com>
2025-02-21 16:34:37 +0530

    Amaan Siddiqui: Add. UC3 Path variable functionality

--- src/main/java/com/example/uc1/hello_api/HelloController.java
+++ src/main/java/com/example/uc1/hello_api/HelloController.java
@@ -1,11 +1,9 @@
 package com.example.uc1.hello_api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-
+@RequestMapping("/hello")
 public class HelloController {
 
     //UC1                                    http://localhost:8080/hello
@@ -17,6 +15,12 @@ public class HelloController {
     //UC2 Passing name as query parameter    http://localhost:8080/query?name=Amaan
     @GetMapping("/query")
     public String sayHelloQuery(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+
+    //UC3 Passing name as path variable      http://localhost:8080/param/Amaan
+    @GetMapping(value = {"/param/{name}", "/uc2"})
+    public String sayHelloPath(@PathVariable String name) {
         return "Hello " + name + " from BridgeLabz";
     }
 
